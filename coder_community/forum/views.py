ba26d4c97992271fea3761aa2e520aff67384e41
Anders Holst <andersthereseholst@gmail.com>
2024-09-21 07:57:57 +0000

    add views

--- coder_community/forum/views.py
+++ coder_community/forum/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Profile, Post, Comment, Like
+from .serializers import ProfileSerializer, PostSerializer, CommentSerializer, LikeSerializer
 
-# Create your views here.
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class LikeViewSet(viewsets.ModelViewSet):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
