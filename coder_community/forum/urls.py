a2af7af55263781f2a4b5a08bac49228abe9fda3
Anders Holst <andersthereseholst@gmail.com>
2024-09-22 13:12:21 +0000

    urls

--- coder_community/forum/urls.py
+++ coder_community/forum/urls.py
@@ -2,15 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProfileViewSet, PostViewSet, CommentViewSet, LikeViewSet, PostListView
 
-# Initialize the DefaultRouter
 router = DefaultRouter()
 router.register(r'profiles', ProfileViewSet)
 router.register(r'posts', PostViewSet)
 router.register(r'comments', CommentViewSet)
 router.register(r'likes', LikeViewSet)
 
-# Define the urlpatterns
 urlpatterns = [
     path('', include(router.urls)),
-    path('posts/', PostListView.as_view(), name='post-list'),
+    path('post-list/', PostListView.as_view(), name='post-list'),
 ]
