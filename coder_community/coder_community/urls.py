24d7ed8671c8220bf6fabe17984471dbdf557c0e
Anders Holst <andersthereseholst@gmail.com>
2024-09-22 13:30:49 +0000

    urls

--- coder_community/coder_community/urls.py
+++ coder_community/coder_community/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', APIRootView.as_view(), name='api-root'),
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='/admin/login/'), name='login'),
+    path('rest-auth/', include('rest_auth.urls')),
     path('logout/', auth_views.LogoutView.as_view(next_page='/admin/login/'), name='logout'),
     path('forum/', include('forum.urls')),
     path('auth/', include('dj_rest_auth.urls')),
